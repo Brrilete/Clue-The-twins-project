0a02f7fd2d65f93b797192619f600c39b5f08912
Brrilete <jsf031994@gmail.com>
2026-06-06 23:08:36 +0100

    fix: add __init__.py to synonyms package

---
+++ IA/synonyms/__init__.py
@@ -0,0 +1,14 @@
+from synonyms.bar import SYNONYMS as S1
+from synonyms.calabozo import SYNONYMS as S2
+from synonyms.casa_muerto import SYNONYMS as S3
+from synonyms.farmacia import SYNONYMS as S4
+from synonyms.hotel import SYNONYMS as S5
+from synonyms.navegacion import SYNONYMS as S6
+
+SYNONYMS = {}
+SYNONYMS.update(S1)
+SYNONYMS.update(S2)
+SYNONYMS.update(S3)
+SYNONYMS.update(S4)
+SYNONYMS.update(S5)
+SYNONYMS.update(S6)
