4d805f06a917b262433099beda868ef5ee623799
Brrilete <jsf031994@gmail.com>
2026-06-07 11:51:09 +0100

    feat: update title and favicon

--- start_tunnel.bat
+++ start_tunnel.bat
@@ -1,4 +1,3 @@
 @echo off
 timeout /t 15 /nobreak
-cloudflared tunnel --url http://localhost:8000 2>&1 | findstr /i "trycloudflare.com" | findstr /i "https" >> C:\Users\jsf03\Desktop\mi_url_ia.txt
 cloudflared tunnel --url http://localhost:8000
